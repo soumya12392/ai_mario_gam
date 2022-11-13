bd3d6a5cf64ef5ed0bc20b7133c39cda08727d19
Soumya Singh <83289414+soumya12392@users.noreply.github.com>
2022-11-13 18:36:33 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,17 @@
+var world_start = "";
+var canvas = "";
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240, 336);
+	instializeInSetup(mario);
+}
+
+function draw() {
+	game()
+}
