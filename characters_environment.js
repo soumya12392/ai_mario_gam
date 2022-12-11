fe1d7c93b5c33b0a128e61b5dd11113d2f9ff1d0
Soumya Singh <83289414+soumya12392@users.noreply.github.com>
2022-12-11 18:27:59 +0530

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -52,8 +52,17 @@ var gameConfig={
 /*====================================
 =            Game Status             =
 ====================================*/
+var noseX = "";
+var noseY = "";
+var GameStatus = "";
 
-function game(){
+function startGame() {
+  GameStatus = "start";
+  document.getElementById("status").innerHTML = "Game is Loading!";
+}
+
+function game() {
+  console.log("noseX = " + noseX + ", noseY = " + noseY);
 
   instializeInDraw();
   moveEnvironment(mario);
