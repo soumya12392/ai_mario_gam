e5716f9498b30f6bef149fc64380eb7d6d46ba2b
Soumya Singh <83289414+soumya12392@users.noreply.github.com>
2022-12-23 19:31:06 +0530

    Add files via upload

--- code.js
+++ code.js
@@ -1,4 +1,9 @@
 var world_start = "";
+var mario_jump = "";
+var mario_coin = "";
+var mario_gameover = "";
+var mario_die = "";
+var mario_kick = "";
 var canvas = "";
 var video = "";
 var poseNet = "";
@@ -7,6 +12,11 @@ var noseY = "";
 
 function preload() {
 	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_die = loadSound("mariodie.wav");
+	mario_kick = loadSound("kick.wav");
 	setSprites();
 	MarioAnimation();
 }
